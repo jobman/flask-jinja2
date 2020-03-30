579cf81ea18e85701ba34ebe6479abf9305dc522
jobman <jobmanalex@gmail.com>
2020-03-30 21:31:28 +0300

    add posts table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -7,6 +8,17 @@ class User(db.Model):
     email = db.Column(db.String(128), index=True, unique=True)
     telegram = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(256))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return 'id({}) username({}) email({}) tg({})'.format(self.id, self.username, self.email, self.telegram)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
