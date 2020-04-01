781c7642876945211a6942fd5b9334fa78e1ba44
jobman <jobmanalex@gmail.com>
2020-04-01 18:35:52 +0300

    user login

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 
 @app.route('/')
@@ -44,9 +46,14 @@ def index():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Запрошен вход для пользователя {} ({}), запомнить = {}'.format(form.username.data, form.password.data,
-                                                                              form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Не правильное имя пользователя или пароль')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Вход', form=form)
