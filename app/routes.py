62d62939e912bfc563b6cc7c6c374e9bfd6fe09a
jobman <jobmanalex@gmail.com>
2020-04-02 12:25:43 +0300

    next page redirect

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'jobman'}
+    if current_user.get_id():
+        user = User.query.get(current_user.get_id())
+        username = user.username
+    else:
+        username = ''
+    user = {'username': username}
     posts = [
         {
             'author': {'username': 'Джанго Фримен'},
@@ -55,8 +62,12 @@ def login():
             flash('Не правильное имя пользователя или пароль')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Вход', form=form)
+
 
 @app.route('/logout')
 def logout():
